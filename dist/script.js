6d91d966740e6c8359a9e4141c5100b53774c9a3
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-06-01 11:34:29 +0200

    two containers
    and somewhat broken version of movement

--- dist/script.js
+++ dist/script.js
@@ -1,22 +1,25 @@
-let state = 50;
+let state = 300;
 
 onkeydown = (event) => {
   const pressedKey = event.key;
-  
+
   if (pressedKey == "a") {
-    state = state - 10;
+    state = state - 50;
   } else if (event.key == "l") {
-    state = state + 10;
+    state = state + 50;
   };
 
-  if (state <= 0) {
+  if (state <= -50) {
     alert("😺 won!");
-    state = 50;
-  } else if (state >= 100) {
+    state = 300;
+  } else if (state >= 750) {
     alert("🐻 won!");
-    state = 50;
+    state = 300;
   };
-  
-  p1.style.top = state + "%";
-  p2.style.top = (100 - state) + "%";
+
+  p1.style.top = state + "px";
+  p2.style.bottom = state + "px";
+
+  console.log(p1.style.top);
+  console.log(p2.style.bottom);
 };
