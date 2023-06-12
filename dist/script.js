54a7d3cf394d7a9a897c381252d37a4978cf7527
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-06-12 14:47:02 +0200

    Final version

--- dist/script.js
+++ dist/script.js
@@ -1,25 +1,58 @@
-let state = 300;
+let state = 334;
+let scoreCat = 0;
+let scoreMonkey = 0;
 
-onkeydown = (event) => {
-  const pressedKey = event.key;
+function delay5() {
+  countdown.innerText = "5 s";
 
-  if (pressedKey == "a") {
-    state = state - 50;
-  } else if (event.key == "l") {
-    state = state + 50;
-  };
+  setTimeout(() => {
+    countdown.innerText = "4 s";
+  }, 1000);
 
-  if (state <= -50) {
-    alert("😺 won!");
-    state = 300;
-  } else if (state >= 750) {
-    alert("🐻 won!");
-    state = 300;
-  };
+  setTimeout(() => {
+    countdown.innerText = "3 s";
+  }, 2000);
 
-  p1.style.top = state + "px";
-  p2.style.bottom = state + "px";
+  setTimeout(() => {
+    countdown.innerText = "2 s";
+  }, 3000);
 
-  console.log(p1.style.top);
-  console.log(p2.style.bottom);
+  setTimeout(() => {
+    countdown.innerText = "1 s";
+  }, 4000);
+ 
+  setTimeout(() => {
+    countdown.innerText = "Jedeme!";
+  }, 5000);
 };
+
+delay5();
+
+setTimeout(() => {
+  onkeydown = (event) => {
+    const pressedKey = event.key;
+
+    if (pressedKey == "a") {
+      state = state - 50;
+    } else if (event.key == "l") {
+      state = state + 50;
+    };
+
+    if (state <= -100) {
+      alert("😺 vyhrála!");
+      state = 334;
+      scoreCat++;
+      scoreCatText.innerText = "😺 " + scoreCat;
+      delay5();
+    } else if (state >= 750) {
+      alert("🐵 vyhrál!");
+      state = 334;
+      scoreMonkey++;
+      scoreMonkeyText.innerText = "🐵 " + scoreMonkey;
+      delay5();
+    };
+
+    p1.style.top = state + "px";
+    p2.style.bottom = state + "px";
+  };
+}, 5000);
